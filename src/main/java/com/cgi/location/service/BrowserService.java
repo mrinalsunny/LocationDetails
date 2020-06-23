dd35dd7ef33a723d54bb6bcc5a7eb2d7076b62f5
mrinalsunny <mrinalsunny@gmail.com>
2020-06-23 21:30:56 +0530

    commit-11

--- src/main/java/com/cgi/location/service/BrowserService.java
+++ src/main/java/com/cgi/location/service/BrowserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cgi.location.model.Browser;
 import com.cgi.location.repo.BrowserRepository;
@@ -23,12 +24,14 @@ public class BrowserService {
 		return repository.findById(id).get();
 	}
 
+	@Transactional
 	public Browser save(Browser browser) {
 		browser.setCreateDate(new Date());
 		browser.setModifiedDate(new Date());
 		return repository.save(browser);
 	}
 
+	@Transactional
 	public Browser update(String id, Browser browser) {
 		Browser toUpdateBrowser = repository.findById(id).get();
 		System.out.println(toUpdateBrowser);
@@ -41,7 +44,8 @@ public class BrowserService {
 		return repository.save(toUpdateBrowser);
 	}
 
+	@Transactional
 	public void delete(String id) {
-		repository.deleteById(id);
+		 repository.deleteById(id);
 	}
 }
